61b1091ad166e10efc29a1841ff20631af5e36b6
DevandScorp <artemda459@gmail.com>
2019-02-07 23:06:30 +0300

    Updates

---
+++ precache-manifest.01333c8f40c08c5e1bbc8ac578a97ca1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "77511caa92db40fe231d",
+    "url": "/rss-mentor-dashboard/static/js/runtime~main.77511caa.js"
+  },
+  {
+    "revision": "d9342613b1a115c29a51",
+    "url": "/rss-mentor-dashboard/static/js/main.d9342613.chunk.js"
+  },
+  {
+    "revision": "08708e86548975bb07ab",
+    "url": "/rss-mentor-dashboard/static/js/1.08708e86.chunk.js"
+  },
+  {
+    "revision": "d9342613b1a115c29a51",
+    "url": "/rss-mentor-dashboard/static/css/main.9a4e5abf.chunk.css"
+  },
+  {
+    "revision": "2e26bcfedbc53366dc965e8ac5fcb90d",
+    "url": "/rss-mentor-dashboard/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/rss-mentor-dashboard/precache-manifest.8657f7fe2623f35f49946c867fdedcea.js"
+  "/rss-mentor-dashboard/precache-manifest.01333c8f40c08c5e1bbc8ac578a97ca1.js"
 );
 
 workbox.clientsClaim();
